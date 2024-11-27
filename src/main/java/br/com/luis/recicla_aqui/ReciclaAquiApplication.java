389351c5fc1200fa83d08fcdb8c066f00b71029a
Luis Henrique <luishenrique2859@hotmail.com>
2024-11-27 12:12:27 -0300

    initial config

---
+++ src/main/java/br/com/luis/recicla_aqui/ReciclaAquiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis.recicla_aqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaAquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luis/recicla_aqui/ReciclaAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis.recicla_aqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
